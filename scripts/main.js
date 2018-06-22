2709d81ecc1af1f45e6b685df0fae9cab25c7e67
John Kennedy <nextechcatapult@arms84.localdomain>
2018-06-22 11:20:58 -0400

    bio

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'HI!, ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'HI!, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
